0f75376ecdced0a1dc658ff6faae1a68a3bb631e
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-08-24 21:36:49 +0400

    lesson 2 some chanegs

--- Level1/Lesson2/Program.cs
+++ Level1/Lesson2/Program.cs
@@ -10,6 +10,26 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
+            byte age; 
+            age = 24;
+
+            string firstName = "Robert";
+            string lastName = "Kocharyan";
+
+            string text = "My name is " + firstName + " " + lastName + " Age " + age;
+
+            byte number1 = 34;
+            byte number2 = 25;
+
+            short number3 = 124;
+            short number4 = 1241;
+
+            int sumShort = number3 + number4;
+            int sum = number1 + number2;
+
+
+            Console.WriteLine(text);
+            Console.ReadKey();
         }
     }
 }
