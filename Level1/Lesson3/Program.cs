abd8a5fcf9bb54ff3365b5719eeda913a34522cd
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-08-26 21:25:29 +0400

    lesson 3 codes

---
+++ Level1/Lesson3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 255;
+
+            bool res = (byte)(b + 1) < b;
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
