35dde3e709181eadf8c8d131cf8fcc881d03d299
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-09-02 21:35:08 +0400

    lesson 6

---
+++ Level1/Lesson6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"Index[{i}], value={array[i]}");
+            }
+
+        }
+    }
+}
