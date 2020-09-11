2d3788fe9f6143b8b412cb269d559d7e656c5c52
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-09-11 21:27:38 +0400

    incapsulatin lesson commited

---
+++ Level1/Lesson9.2/Car.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9._2
+{
+    public class Car
+    {
+        private ushort _maxSpeed;
+        private string _color;
+        private string _class;
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+
+        private string _engineType;
+        private string _model;
+
+        public Car(string engineType, ushort maxSpeed,
+            string color, string @class)
+        {
+            _engineType = engineType;
+            _maxSpeed = maxSpeed;
+            _color = color;
+            _class = @class;
+        }
+
+        public int Move(byte transmission)
+        {
+            int speed = MoveLogic(transmission);
+            return speed;
+        }
+
+        public bool IsStopped(int speed)
+        {
+            return speed == 0;
+        }
+
+        private int MoveLogic(byte transmission)
+        {
+            if (transmission == 1)
+            {
+                return 30;
+            }
+            if (transmission == 2)
+            {
+                return 60;
+            }
+            if (transmission == 3)
+            {
+                return 80;
+            }
+            if (transmission == 4)
+            {
+                return 130;
+            }
+            if (transmission == 4)
+            {
+                return 200;
+            }
+
+            return 0;
+        }
+
+        public string GetEngineType()
+        {
+            return _engineType;
+        }
+
+    }
+}
---
+++ Level1/Lessson9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson9._2;
+
+
+namespace Lessson9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car("8 cylinder", 300, "BMW", "White");
+            bmw.Model = "3 series";//set
+
+            Console.WriteLine(bmw.Model);//get
+
+
+            string engineType = bmw.GetEngineType();
+
+            Console.WriteLine(engineType);
+
+            var isStopped = bmw.IsStopped(50);
+            Console.WriteLine($"Car is stopped: {isStopped}");
+        }
+    }
+}
